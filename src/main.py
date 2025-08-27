662155b5a27bc02d366f9f7300a25662891bb961
Lonnie Currie <lonnie.d.currie@gmail.com>
2025-08-26 22:28:46 -0500

    updating to support repo build

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ from textnode import TextType, TextNode
 from mdconversion import *
 from copy_static import copy_static_files
 
+STATIC_PATH = "static"
+CONTENT_PATH = "content"
+TEMPLATE_PATH = "template.html"
+DEST_PATH = "docs"
+
 def main():
 
     if len(sys.argv) > 1:
@@ -12,7 +17,7 @@ def main():
     else:
         base_path = "/"
 
-    copy_static_files(STATIC_PATH, PUBLIC_PATH)
+    copy_static_files(STATIC_PATH, DEST_PATH)
     generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, DEST_PATH, base_path)
 
 main()

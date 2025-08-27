9e6bdc64925f8932548d9fd65fc5890d9a3a0c1d
Lonnie Currie <lonnie.d.currie@gmail.com>
2025-08-26 23:00:02 -0500

    more testing

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-python3 src/main.py "/https://github.com/SoFarCalm/static-site-gen/"
+python3 src/main.py "/static-site-gen/"
--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ from textnode import TextType, TextNode
 from mdconversion import *
 from copy_static import copy_static_files
 
-STATIC_PATH = "/static"
-CONTENT_PATH = "/content"
+STATIC_PATH = "static"
+CONTENT_PATH = "content"
 TEMPLATE_PATH = "template.html"
-DEST_PATH = "/docs"
+DEST_PATH = "docs"
 
 def main():
 
     if len(sys.argv) > 1:
-        base_path = sys.argv[0]
+        base_path = sys.argv[1]
     else:
         base_path = "/"
 
